c49788e85b19800a3a6e3137850dde023e1c229a
sr1k5 <srikanthragh06@gmail.com>
2023-10-07 18:53:28 +0530

    removed score typing effect

--- index.js
+++ index.js
@@ -1,61 +1,62 @@
-
-function generateCPUMove(moves){
-    const moveFloat=Math.random()*3;
-    if (moveFloat<1) {
-        moves.cpuMove='r'
-    }
-    else if (moveFloat>=1 && moveFloat<2) 
-    { 
-        moves.cpuMove='p' 
-    }
-    else{ 
-        moves.cpuMove='s' 
+function generateCPUMove(moves) {
+    const moveFloat = Math.random() * 3;
+    if (moveFloat < 1) {
+        moves.cpuMove = "r";
+    } else if (moveFloat >= 1 && moveFloat < 2) {
+        moves.cpuMove = "p";
+    } else {
+        moves.cpuMove = "s";
     }
 }
 
-function getResult(moves){
-    if (moves.userMove==moves.cpuMove){
+function getResult(moves) {
+    if (moves.userMove == moves.cpuMove) {
         return 0;
-    }
-    else if ((moves.userMove=='r' && moves.cpuMove=='s')||(moves.userMove=='p' && moves.cpuMove=='r')||(moves.userMove=='s' && moves.cpuMove=='p')){
+    } else if (
+        (moves.userMove == "r" && moves.cpuMove == "s") ||
+        (moves.userMove == "p" && moves.cpuMove == "r") ||
+        (moves.userMove == "s" && moves.cpuMove == "p")
+    ) {
         return 1;
-    }
-    else{
+    } else {
         return -1;
     }
 }
 
-
-
-
-function main(){
-
-    
+function main() {
     // tag initialization
-    const footer=document.querySelector('.footer');
-    const userScoreDisplay=document.querySelector('.score__user-score');
-    const cpuScoreDisplay=document.querySelector('.score__cpu-score');
-    const header=document.querySelector('.header');
-    const middleTitle=document.querySelector('.middle__title-container')
-    const middleResult=document.querySelector('.middle__result-container');
-    const middleWaiting=document.querySelector('.middle__waiting-container');
-    const userHands=document.querySelectorAll(".userHands");
-    const stageHand1=document.querySelector('.middle__result-container__stage__hand1');
-    const stageHand2=document.querySelector('.middle__result-container__stage__hand2');
-    const resultUpdateText=document.querySelector('.middle__result-container__updation-text');
-    const resultDetailsText=document.querySelector('.middle__result-container__details-text');
+    const footer = document.querySelector(".footer");
+    const userScoreDisplay = document.querySelector(".score__user-score");
+    const cpuScoreDisplay = document.querySelector(".score__cpu-score");
+    const header = document.querySelector(".header");
+    const middleTitle = document.querySelector(".middle__title-container");
+    const middleResult = document.querySelector(".middle__result-container");
+    const middleWaiting = document.querySelector(".middle__waiting-container");
+    const userHands = document.querySelectorAll(".userHands");
+    const stageHand1 = document.querySelector(
+        ".middle__result-container__stage__hand1"
+    );
+    const stageHand2 = document.querySelector(
+        ".middle__result-container__stage__hand2"
+    );
+    const resultUpdateText = document.querySelector(
+        ".middle__result-container__updation-text"
+    );
+    const resultDetailsText = document.querySelector(
+        ".middle__result-container__details-text"
+    );
 
     //variables initialization
-    
-    const score={userScore:0,cpuScore:0};
-    
-    const moves={userMove:'r',cpuMove:'r'};
-    let needHandListeners=false;
-    let isHandListenersAdded=false;
-    let isHandListenersRemoved=false;
-    
-    let needResults=false;
-    let isResultRunning=false;
+
+    const score = { userScore: 0, cpuScore: 0 };
+
+    const moves = { userMove: "r", cpuMove: "r" };
+    let needHandListeners = false;
+    let isHandListenersAdded = false;
+    let isHandListenersRemoved = false;
+
+    let needResults = false;
+    let isResultRunning = false;
 
     // Execution
 
@@ -66,135 +67,146 @@ function main(){
     //hideDiv(middleTitle);
     hideDiv(middleWaiting);
     hideDiv(middleResult);
-    
-    middleTitle.addEventListener('click',function(){
+
+    middleTitle.addEventListener("click", function () {
         hideDiv(footer);
         hideDiv(middleTitle);
-        addTypeWriterEffect(middleWaiting,"make your move...",'flex');
-        addTypeWriterEffect(userScoreDisplay,"user_score: 0",'block');
-        addTypeWriterEffect(cpuScoreDisplay,"cpu_score: 0",'block');
-        showDiv(header,'flex');
+        addTypeWriterEffect(middleWaiting, "make your move...", "flex");
+        // addTypeWriterEffect(userScoreDisplay, "user_score: 0", "block");
+        // addTypeWriterEffect(cpuScoreDisplay, "cpu_score: 0", "block");
+        showDiv(userScoreDisplay, "block");
+        showDiv(cpuScoreDisplay, "block");
+        showDiv(header, "flex");
         needHandListeners = true;
     });
 
     const clickUserHands = [
-        function() {
+        function () {
             console.log("user rock clicked");
-            moves.userMove='r';
+            moves.userMove = "r";
             generateCPUMove(moves);
-            needHandListeners=false;
-            needResults=true;
+            needHandListeners = false;
+            needResults = true;
         },
-        function() {
+        function () {
             console.log("user paper clicked");
-            moves.userMove='p';
+            moves.userMove = "p";
             generateCPUMove(moves);
-            needHandListeners=false;
-            needResults=true;
+            needHandListeners = false;
+            needResults = true;
         },
-        function() {
+        function () {
             console.log("user scissors clicked");
-            moves.userMove='s';
+            moves.userMove = "s";
             generateCPUMove(moves);
-            needHandListeners=false;
-            needResults=true;
+            needHandListeners = false;
+            needResults = true;
         },
     ];
-      
-    setInterval(function() {
-        
+
+    setInterval(function () {
         if (needHandListeners && !isHandListenersAdded) {
             console.log("hand listeners added");
-            
-            for ( i=0;i<userHands.length;i++){
-                userHands[i].addEventListener('click', clickUserHands[i]);
+
+            for (i = 0; i < userHands.length; i++) {
+                userHands[i].addEventListener("click", clickUserHands[i]);
             }
 
-            isHandListenersAdded=true;
-            isHandListenersRemoved=false;
-        } 
-        else if (!needHandListeners && !isHandListenersRemoved) {
+            isHandListenersAdded = true;
+            isHandListenersRemoved = false;
+        } else if (!needHandListeners && !isHandListenersRemoved) {
             console.log("hand listeners removed");
-            
-            for ( i=0;i<userHands.length;i++){
-                userHands[i].removeEventListener('click', clickUserHands[i]);
+
+            for (i = 0; i < userHands.length; i++) {
+                userHands[i].removeEventListener("click", clickUserHands[i]);
             }
 
             isHandListenersRemoved = true;
-            isHandListenersAdded=false;
+            isHandListenersAdded = false;
         }
     }, 25);
 
-    function placeResultImgs(){
-        let imgDiv=stageHand1.querySelector('img');
-        if (moves.userMove=='r'){
-            imgDiv.src='img/rock_right.png';
-        }
-        else if (moves.userMove=='p'){
-            imgDiv.src='img/paper_right.png';
-        }
-        else {
-            imgDiv.src='img/scissors_right.png';
+    function placeResultImgs() {
+        let imgDiv = stageHand1.querySelector("img");
+        if (moves.userMove == "r") {
+            imgDiv.src = "img/rock_right.png";
+        } else if (moves.userMove == "p") {
+            imgDiv.src = "img/paper_right.png";
+        } else {
+            imgDiv.src = "img/scissors_right.png";
         }
 
-        imgDiv=stageHand2.querySelector('img');
-        if (moves.cpuMove=='r'){
-            imgDiv.src='img/rock_left.png';
-        }
-        else if (moves.cpuMove=='p'){
-            imgDiv.src='img/paper_left.png';
-        }
-        else {
-            imgDiv.src='img/scissors_left.png';
+        imgDiv = stageHand2.querySelector("img");
+        if (moves.cpuMove == "r") {
+            imgDiv.src = "img/rock_left.png";
+        } else if (moves.cpuMove == "p") {
+            imgDiv.src = "img/paper_left.png";
+        } else {
+            imgDiv.src = "img/scissors_left.png";
         }
     }
 
-    function updateResultDetails(){
-        if (moves.userMove==moves.cpuMove) {
-            addTypeWriterEffect(resultDetailsText,"well that's a draw",'block');
-        }
-        else if ((moves.userMove=='r' && moves.cpuMove=='p')||(moves.userMove=='p' && moves.cpuMove=='r')){
-            addTypeWriterEffect(resultDetailsText,"paper beats rock!!",'block');
-        }
-        else if ((moves.userMove=='r' && moves.cpuMove=='s')||(moves.userMove=='s' && moves.cpuMove=='r')){
-            addTypeWriterEffect(resultDetailsText,"rock beats scissors!!",'block');
-        }
-        else if ((moves.userMove=='s' && moves.cpuMove=='p')||(moves.userMove=='p' && moves.cpuMove=='s')){
-            addTypeWriterEffect(resultDetailsText,"scissors beats paper!!",'block');
-        }
-
-    }
-    
-    function updateResultUpdate(res){
-        if (res==1){
-            addTypeWriterEffect(resultUpdateText,"user_score++;",'block');
-        }
-        else if(res==0){
-            addTypeWriterEffect(resultUpdateText,"hmmmm...",'block');
-        }
-        else{
-            addTypeWriterEffect(resultUpdateText,"cpu_score++;",'block');
-        }
-    }
-
-    function updateScore(res){
-
-        if (res==1){
-            let scoreDigits=findDigits(score.userScore);
-            score.userScore+=1;
-            updateTypeWriterEffect(userScoreDisplay,score.userScore.toString(),'block',scoreDigits,750);
-        }
-        else if(res==-1){
-            let scoreDigits=findDigits(score.cpuScore);
-            score.cpuScore+=1;
-            updateTypeWriterEffect(cpuScoreDisplay,score.cpuScore.toString(),'block',scoreDigits,750);
+    function updateResultDetails() {
+        if (moves.userMove == moves.cpuMove) {
+            addTypeWriterEffect(
+                resultDetailsText,
+                "well that's a draw",
+                "block"
+            );
+        } else if (
+            (moves.userMove == "r" && moves.cpuMove == "p") ||
+            (moves.userMove == "p" && moves.cpuMove == "r")
+        ) {
+            addTypeWriterEffect(
+                resultDetailsText,
+                "paper beats rock!!",
+                "block"
+            );
+        } else if (
+            (moves.userMove == "r" && moves.cpuMove == "s") ||
+            (moves.userMove == "s" && moves.cpuMove == "r")
+        ) {
+            addTypeWriterEffect(
+                resultDetailsText,
+                "rock beats scissors!!",
+                "block"
+            );
+        } else if (
+            (moves.userMove == "s" && moves.cpuMove == "p") ||
+            (moves.userMove == "p" && moves.cpuMove == "s")
+        ) {
+            addTypeWriterEffect(
+                resultDetailsText,
+                "scissors beats paper!!",
+                "block"
+            );
         }
     }
 
-    function runResults(){
-        isResultRunning=true;
+    function updateResultUpdate(res) {
+        if (res == 1) {
+            addTypeWriterEffect(resultUpdateText, "user_score++;", "block");
+        } else if (res == 0) {
+            addTypeWriterEffect(resultUpdateText, "hmmmm...", "block");
+        } else {
+            addTypeWriterEffect(resultUpdateText, "cpu_score++;", "block");
+        }
+    }
 
-        
+    function updateScore(res) {
+        if (res == 1) {
+            let scoreDigits = findDigits(score.userScore);
+            score.userScore += 1;
+            userScoreDisplay.innerHTML = `user_score: ${score.userScore}`;
+        } else if (res == -1) {
+            let scoreDigits = findDigits(score.cpuScore);
+            score.cpuScore += 1;
+            cpuScoreDisplay.innerHTML = `cpu_score: ${score.cpuScore}`;
+        }
+    }
+
+    function runResults() {
+        isResultRunning = true;
 
         setTimeout(() => {
             hideDiv(middleResult);
@@ -202,34 +214,30 @@ function main(){
             emptyDivTypeWriterText(resultUpdateText);
 
             emptyDivTypeWriterText(middleWaiting);
-            showDiv(middleWaiting,'flex');
-            addTypeWriterEffect(middleWaiting,"make your move...",'flex');
-            needHandListeners=true;
+            showDiv(middleWaiting, "flex");
+            addTypeWriterEffect(middleWaiting, "make your move...", "flex");
+            needHandListeners = true;
         }, 2500);
 
         hideDiv(middleWaiting);
         placeResultImgs(moves);
-        let res=getResult(moves);
+        let res = getResult(moves);
         setTimeout(() => {
-            
             updateResultUpdate(res);
         }, 600);
         updateResultDetails();
-        
+
         updateScore(res);
 
-        
-        showDiv(middleResult,'flex');
-        isResultRunning=false;
+        showDiv(middleResult, "flex");
+        isResultRunning = false;
     }
 
-    setInterval(function() {
-        if (needResults && !isResultRunning){
+    setInterval(function () {
+        if (needResults && !isResultRunning) {
             runResults();
-            needResults=false;
+            needResults = false;
         }
-    },25);
-    
-
+    }, 25);
 }
 main();
--- utils.js
+++ utils.js
@@ -1,99 +1,97 @@
-function findDigits(n){
-    if (n==0){
+function findDigits(n) {
+    if (n == 0) {
         return 1;
     }
-    let count=0;
-    while (n>0){
+    let count = 0;
+    while (n > 0) {
         count++;
-        n=Math.floor(n/10);
+        n = Math.floor(n / 10);
     }
     return count;
 }
 
-function hideDiv(div){
-    div.style.display = 'none';
+function hideDiv(div) {
+    div.style.display = "none";
 }
 
-function showDiv(div,display){
+function showDiv(div, display) {
     div.style.display = display;
 }
 
-function emptyDivTypeWriterText(div){
-    typeWriterText=div.querySelector('.typewriter');
-    typeWriterText.textContent='';
+function emptyDivTypeWriterText(div) {
+    typeWriterText = div.querySelector(".typewriter");
+    typeWriterText.textContent = "";
 }
 
-function removeAllBorder(){
+function removeAllBorder() {
     // Get all elements on the page
-    var allElements = document.querySelectorAll('*');
+    var allElements = document.querySelectorAll("*");
 
     // Loop through each element and remove the border
     for (var i = 0; i < allElements.length; i++) {
-        allElements[i].style.border = 'none';
+        allElements[i].style.border = "none";
     }
 }
 
-function addTypeWriterEffect(div,phrase,display='block',speed=50){
-
-    div.style.display=display;
-    const typeWriterDiv=div.querySelector('.typewriter');
-    
+function addTypeWriterEffect(div, phrase, display = "block", speed = 50) {
+    div.style.display = display;
+    const typeWriterDiv = div.querySelector(".typewriter");
 
     if (!typeWriterDiv) {
         console.error("Element with class 'typewriter' not found.");
         return; // Exit the function if the element is not found
     }
 
-    let i=0;
-    let intervalID=setInterval(function(){
-    if (i<phrase.length){
-        typeWriterDiv.textContent+=phrase[i];
-        i++;
-    }
-    else{
-        clearInterval(intervalID);
-    }
-    },speed);
+    let i = 0;
+    let intervalID = setInterval(function () {
+        if (i < phrase.length) {
+            typeWriterDiv.textContent += phrase[i];
+            i++;
+        } else {
+            clearInterval(intervalID);
+        }
+    }, speed);
 }
 
-function eraseTypeWriterEffect(div,eraseIterations=1,speed=50){
-    const typeWriterDiv=div.querySelector('.typewriter');
+function eraseTypeWriterEffect(div, eraseIterations = 1, speed = 50) {
+    const typeWriterDiv = div.querySelector(".typewriter");
 
     if (!typeWriterDiv) {
         console.error("Element with class 'typewriter' not found.");
         return; // Exit the function if the element is not found
     }
 
-    let newText=typeWriterDiv.textContent;
-    let intervalID=setInterval(function(){
-    if (eraseIterations>0){
-        newText=newText.slice(0,-1);
-        typeWriterDiv.textContent=newText
-        eraseIterations-=1
-    }
-    else{
-        clearInterval(intervalID);
-    }
-    },speed);
+    let newText = typeWriterDiv.textContent;
+    let intervalID = setInterval(function () {
+        if (eraseIterations > 0) {
+            newText = newText.slice(0, -1);
+            typeWriterDiv.textContent = newText;
+            eraseIterations -= 1;
+        } else {
+            clearInterval(intervalID);
+        }
+    }, speed);
 }
 
-function updateTypeWriterEffect(div,phrase,display='block',eraseIterations=1,speed=50){
-    eraseTypeWriterEffect(div,eraseIterations,speed);
-    setTimeout(function(){
-        addTypeWriterEffect(div,phrase,display,speed);
-    }
-    ,speed*eraseIterations);
-    
+function updateTypeWriterEffect(
+    div,
+    phrase,
+    display = "block",
+    eraseIterations = 1,
+    speed = 50
+) {
+    eraseTypeWriterEffect(div, eraseIterations, speed);
+    setTimeout(function () {
+        addTypeWriterEffect(div, phrase, display, speed);
+    }, speed * eraseIterations);
 }
 
-function fadeOut(div){
-    
-    div.classList.add('fade-out');
+function fadeOut(div) {
+    div.classList.add("fade-out");
     // div.style.display='none';
 }
 
-function fadeIn(div,display){
-    div.classList.add('fade-in');
+function fadeIn(div, display) {
+    div.classList.add("fade-in");
     // div.style.display = display;
 }
-
